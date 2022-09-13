193134c8ea2db667e730a001db2c86191b305287
Ryan <ryjen.pears@gmail.com>
2022-09-13 10:37:27 +0200

    guessing game added

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,21 @@
 function guessingGame() {
+  const answer = Math.floor(Math.random() * 100);
+  let isOver = false;
+  let numGuesses = 0;
 
+  return function guess(num) {
+    if (isOver) return `The game is over, you already won!`;
+    numGuesses++;
+
+    if (num === answer) {
+      isOver = true;
+      const guess = numGuesses === 1 ? 'guess' : 'guesses';
+      return `You win! You found ${num} in ${numGuesses} ${guess}.`;
+    }
+
+    if (num < answer) return `${num} is too low!`;
+    if (num > answer) return `${num} is too high!`;
+  }
 }
 
 module.exports = { guessingGame };
